214d549d42048af762ad6773bc595e97228b19b2
Abadie patrick <abadie.patrick27@gmail.com>
2026-06-11 18:06:25 +0200

    Fix import case sensitivity for page components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { BrowserRouter, Routes, Route } from "react-router";
-import Home from './pages/home.jsx'
+import Home from './pages/Home.jsx'
 import Logement from './pages/Logement.jsx'
-import Error404 from './pages/error_404.jsx'
+import Error404 from './pages/Error_404.jsx'
 import About from './pages/About.jsx'
 
 const root = document.getElementById("root");
